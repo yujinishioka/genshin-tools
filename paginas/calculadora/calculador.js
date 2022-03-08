b2059e7ce4187b3eb7a9509cb2e6d96c0b1f5481
yuji nishioka <yuu.nishioka>
2022-03-07 22:34:55 -0300

    iniciando calculador de status

--- paginas/calculadora/calculador.js
+++ paginas/calculadora/calculador.js
@@ -4,3 +4,16 @@ const artefato = require('../../json/dados-artefatos.json');
 function buscar(){
     console.log(personagem)
 }
+
+function calcular(analise){
+    for(i=0; i<artefato.length; i++){
+        if(i==0){
+            console.log(analise.ATK[13])
+            analise.ATK[13] += (artefato[0].set2 * analise.ATK[13] / 100)
+            console.log(analise.ATK[13])
+            console.log(artefato[0].set2)
+        }
+    }
+}
+
+calcular(personagem[0])
